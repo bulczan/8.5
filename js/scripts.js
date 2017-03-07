db1c2fe05d32a370dd49e50f4fec1b5ddf8c211e
KatarzynaAndrys <kat.andrys@gmail.com>
2017-03-07 23:08:16 +0100

    Poprawione

--- js/scripts.js
+++ js/scripts.js
@@ -8,9 +8,8 @@ if (value > 0) {
 	console.log('Wynik dodatni')
 } else if (value < 0) {
 	console.log('Wynik ujemny')
-} 
-
-switch (value) {
-	case 0:
-	alert('value wynosi 0')
+} else if (value = 0) {
+	console.log ('Wynik to 0')
 }
+
+
